d8b852e5632a3d00d4f052e6d52aef13abc7e37d
Lu <gl8997@gmail.com>
2016-11-01 22:44:25 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -21,7 +21,7 @@ function message() {
 }
 
 # Files
-REMOTE='https://raw.githubusercontent.com/denysdovhan/spaceship-zsh-theme/master/spaceship.zsh'
+REMOTE='https://raw.githubusercontent.com/lgan1989/spaceship-zsh-theme/master/spaceship.zsh'
 THEME="$ZSH_CUSTOM/themes/spaceship.zsh-theme"
 
 # If themes folder isn't exist, then make it
